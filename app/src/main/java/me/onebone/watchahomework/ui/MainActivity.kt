fbac3eb74271d84e044804f0a9b6a21fcb945433
onebone <jyc00410@gmail.com>
2021-07-24 19:44:20 +0900

    feat: connect bottom navigation with view pager

--- app/src/main/java/me/onebone/watchahomework/ui/MainActivity.kt
+++ app/src/main/java/me/onebone/watchahomework/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package me.onebone.watchahomework.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
 import me.onebone.watchahomework.R
 import me.onebone.watchahomework.databinding.ActivityMainBinding
@@ -16,5 +17,25 @@ class MainActivity: AppCompatActivity() {
 
 		val adapter = PagerAdapter(this)
 		binding.pager.adapter = adapter
+
+		binding.bottomNavigation.setOnItemSelectedListener {
+			binding.pager.currentItem = when(it.itemId) {
+				R.id.menu_tracks -> 0
+				R.id.menu_favorites -> 1
+				else -> throw IllegalArgumentException("invalid item id")
+			}
+
+			true
+		}
+
+		binding.pager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+			override fun onPageSelected(position: Int) {
+				binding.bottomNavigation.menu.getItem(when(position) {
+					0 -> 0
+					1 -> 1
+					else -> throw IllegalArgumentException("invalid position given: $position")
+				}).isChecked = true
+			}
+		})
 	}
 }
