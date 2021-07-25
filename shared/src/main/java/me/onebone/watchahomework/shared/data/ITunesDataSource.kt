90be462e38a6d3822cf8dc87e487fdebe9e3a1d5
onebone <jyc00410@gmail.com>
2021-07-25 19:18:14 +0900

    refactor: use library function rather than manual implementation

--- shared/src/main/java/me/onebone/watchahomework/shared/data/ITunesDataSource.kt
+++ shared/src/main/java/me/onebone/watchahomework/shared/data/ITunesDataSource.kt
@@ -1,16 +1,14 @@
 package me.onebone.watchahomework.shared.data
 
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.serialization.responseObject
-import com.github.kittinunf.result.Result as FuelResult
-import kotlinx.coroutines.suspendCancellableCoroutine
+import com.github.kittinunf.fuel.core.Deserializable
+import com.github.kittinunf.fuel.core.awaitResponse
+import com.github.kittinunf.fuel.serialization.kotlinxDeserializerOf
 import kotlinx.serialization.json.Json
 import me.onebone.watchahomework.model.Track
 import me.onebone.watchahomework.model.response.SearchResultResponse
 import me.onebone.watchahomework.shared.BuildConfig
 import javax.inject.Inject
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 
 interface ITunesDataSource {
 	suspend fun getTracks(offset: Int, limit: Int): List<Track>
@@ -19,24 +17,15 @@ interface ITunesDataSource {
 class FuelITunesDataSource @Inject constructor(
 	private val json: Json
 ): ITunesDataSource {
-	override suspend fun getTracks(offset: Int, limit: Int): List<Track> = suspendCancellableCoroutine { continuation ->
-		val request = Fuel.get(
+	override suspend fun getTracks(offset: Int, limit: Int): List<Track> {
+		val (_, _, data) = Fuel.get(
 			path = "${BuildConfig.ITUNES_API_BASE_URL}/search",
 			parameters = listOf(
 				"offset" to offset, "limit" to limit,
 				"term" to "greenday", "entity" to "song"
 			)
-		).responseObject<SearchResultResponse>(json) { _, _, result ->
-			when(result) {
-				is FuelResult.Success ->
-					continuation.resume(result.value.results)
-				is FuelResult.Failure ->
-					continuation.resumeWithException(result.getException())
-			}
-		}
+		).awaitResponse<SearchResultResponse, Deserializable<SearchResultResponse>>(kotlinxDeserializerOf(json))
 
-		continuation.invokeOnCancellation {
-			request.cancel()
-		}
+		return data.results
 	}
 }
